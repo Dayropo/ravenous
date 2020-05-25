90806ff42c604f5c10fbe0f3ca080e76c98c99f1
Gboyega Oyebanjo <gboyega.banjo@yahoo.com>
2020-05-25 20:46:14 +0100

    Updates

---
+++ precache-manifest.2b3d3c4ff43362f603abe675fdecbcc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa941d6179092e39486a2e34c10d2adc",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "0fa70c692c03f9cc8163",
+    "url": "/ravenous/static/css/main.f205d4fb.chunk.css"
+  },
+  {
+    "revision": "5b631a7ba56a5f56da04",
+    "url": "/ravenous/static/js/2.70a0e2c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.70a0e2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa70c692c03f9cc8163",
+    "url": "/ravenous/static/js/main.5febbbfb.chunk.js"
+  },
+  {
+    "revision": "f84e9b78bea11c292415",
+    "url": "/ravenous/static/js/runtime-main.4069afa6.js"
+  }
+]);
